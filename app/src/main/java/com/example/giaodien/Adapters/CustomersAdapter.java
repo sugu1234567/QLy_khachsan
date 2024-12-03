6d7a1ccaef2614d1cef09a691ee394c27c6c2bf9
procoder0-0 <124030601+procoder0-0@users.noreply.github.com>
2024-12-03 09:23:40 +0700

    Update CustomersAdapter.java

--- app/src/main/java/com/example/giaodien/Adapters/CustomersAdapter.java
+++ app/src/main/java/com/example/giaodien/Adapters/CustomersAdapter.java
@@ -105,7 +105,8 @@ public class CustomersAdapter extends RecyclerView.Adapter<CustomersAdapter.Cust
             public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
                 // Kiểm tra từng ký tự có phải là chữ không
                 for (int i = start; i < end; i++) {
-                    if (!Character.isLetter(source.charAt(i))) {
+                    char c = source.charAt(i);
+                    if (!Character.isLetter(c) && c != ' ') {
                         return "";
                     }
                 }
